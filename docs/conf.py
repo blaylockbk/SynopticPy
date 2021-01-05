412e97ec030d1afe52bde630e5b407acd1f8b0ec
Brian Blaylock <blaylockbk@gmail.com>
2021-01-05 23:59:53 +0000

    Updated Sphinx Docs

--- docs/conf.py
+++ docs/conf.py
@@ -65,9 +65,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',
 #html_theme = "sphinx_rtd_theme"
 html_theme = "pydata_sphinx_theme"
 html_theme_options = {
-    'logo_only': False,
-    'navigation_depth': 5,
-    'collapse_navigation': False,
+    'github_url': 'https://github.com/blaylockbk/SynopticPy',
+    #'logo_only': False,
+    #'navigation_depth': 5,
+    #'collapse_navigation': False,
 }
 
 #html_logo = "_static/WeatherBalloon.gif"
