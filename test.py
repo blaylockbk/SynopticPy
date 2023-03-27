b8d135a0da50643c3002dca084c960f79be6993e
Weidav <david.weissensteiner@gmail.com>
2023-03-27 14:24:12 +0000

    test

--- test.py
+++ test.py
@@ -5,19 +5,13 @@ import pprint
 from datetime import timedelta
 from synoptic.services import stations_metadata, stations_timeseries
 
-env_var = os.environ
-
-# Print the list of user's
-# environment variables
-print("User's Environment variable:")
-pprint.pprint(dict(env_var), width=1)
 
 # in GitPod case the config file is saved here:
 # /home/gitpod/.config/SynopticPy/config.toml
 
-# df = stations_timeseries(
-#     stid="WBB", vars=["air_temp", "wind_speed"], recent=timedelta(hours=1)
-# )
+df = stations_timeseries(
+    stid="WBB", vars=["air_temp", "wind_speed"], recent=timedelta(hours=1)
+)
 
-# print(df)
+print(df)
 
