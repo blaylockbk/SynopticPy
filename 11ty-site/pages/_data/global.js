f8c9184afdf3d1f49ee2677ea4f0f45d10070381
Nathan Blaylock <ngblaylock@gmail.com>
2023-07-04 13:00:56 -0600

    improve workflow

--- 11ty-site/pages/_data/global.js
+++ 11ty-site/pages/_data/global.js
@@ -2,5 +2,6 @@ module.exports = {
   env: process.env.ELEVENTY_ENV,
   root: process.env.ELEVENTY_ENV === 'dev' ? '/app' : '/SynopticPy/app',
   year: new Date().getFullYear(),
-  renderedDate: new Date()
+  renderedDate: new Date(),
+  allowPyScript: process.env.ALLOW_PY_SCRIPT === '1' ? true : false,
 }
