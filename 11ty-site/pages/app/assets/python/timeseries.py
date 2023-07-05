aae02cd33c07f971ae71e0e3fee31072c95e3108
Brian Blaylock <blaylockbk@gmail.com>
2023-07-04 18:15:51 -0700

    fixed custom font for matplotlib

--- 11ty-site/pages/app/assets/python/timeseries.py
+++ 11ty-site/pages/app/assets/python/timeseries.py
@@ -19,7 +19,15 @@ print(f"imported matplotlib {mpl.__version__}")
 print(f"imported pandas {pd.__version__}")
 print(f"imported numpy {np.__version__}")
 
-for font in mpl.font_manager.findSystemFonts(["../fonts"]):
+
+# from pathlib import Path
+# print("Current Directory: ", Path("./").resolve())
+# print("   Contents:", list(Path("./").resolve().glob("*")))
+# print("Back one Directory: ", Path("../").resolve())
+# print("   Contents:", list(Path("../").resolve().glob("*")))
+
+
+for font in mpl.font_manager.findSystemFonts(["../assets/fonts"]):
     mpl.font_manager.fontManager.addfont(font)
 plt.rcParams["font.sans-serif"] = ["Mona Sans", "Mona-Sans", "Hubot-Sans"]
 mpl.rcParams["date.autoformatter.day"] = "%b %d\n%Y"
