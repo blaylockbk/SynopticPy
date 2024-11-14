3f8b0e1df11680a7d63bde09218cbc033b8be6be
Brian Blaylock <blaylockbk@gmail.com>
2024-11-13 20:38:16 -0800

    fixes Error converting LATITUDE from string to float #62

--- src/synoptic/json_parsers.py
+++ src/synoptic/json_parsers.py
@@ -35,6 +35,7 @@ def station_metadata_to_dataframe(metadata: dict) -> pl.DataFrame:
     metadata.pop("QC", None)
     df = (
         pl.DataFrame(metadata)
+        .with_columns(pl.col("LATITUDE", "LONGITUDE").str.strip_chars())
         .with_columns(
             pl.col("STID").cast(pl.String),
             pl.col("ID", "MNET_ID").cast(pl.UInt32),
